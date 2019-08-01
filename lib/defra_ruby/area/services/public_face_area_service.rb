85d843bd60dd46d9bcb7c2220ac7b65c3a39516f
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2019-08-01 09:40:39 +0100

    Only return long name in public face lookup
    We only use the long name in the services that use these area lookups, so were reducing the load and complexity of the url by specifying we only want the long name value returned.

--- lib/defra_ruby/area/services/public_face_area_service.rb
+++ lib/defra_ruby/area/services/public_face_area_service.rb
@@ -16,7 +16,7 @@ module DefraRuby
 
       def url
         # rubocop:disable Metrics/LineLength
-        "https://environment.data.gov.uk/spatialdata/administrative-boundaries-environment-agency-and-natural-england-public-face-areas/wfs?SERVICE=WFS&VERSION=2.0.0&REQUEST=GetFeature&typeNames=ms:Administrative_Boundaries_Environment_Agency_and_Natural_England_Public_Face_Areas&propertyName=long_name,short_name&SRSName=EPSG:27700&Filter=(<Filter><Intersects><PropertyName>SHAPE</PropertyName><gml:Point><gml:coordinates>#{easting},#{northing}</gml:coordinates></gml:Point></Intersects></Filter>)"
+        "https://environment.data.gov.uk/spatialdata/administrative-boundaries-environment-agency-and-natural-england-public-face-areas/wfs?SERVICE=WFS&VERSION=2.0.0&REQUEST=GetFeature&typeNames=ms:Administrative_Boundaries_Environment_Agency_and_Natural_England_Public_Face_Areas&propertyName=long_name&SRSName=EPSG:27700&Filter=(<Filter><Intersects><PropertyName>SHAPE</PropertyName><gml:Point><gml:coordinates>#{easting},#{northing}</gml:coordinates></gml:Point></Intersects></Filter>)"
         # rubocop:enable Metrics/LineLength
       end
 
