dbdaaf4d45864aac827de558de494be762282ad3
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2019-08-03 10:59:13 +0100

    Fix broken public face area service
    Whilst updating the README (coming in a future commit) we ran the query for the public face service manully using CURL and got no response.
    
    When implementing the Water Management service we initially just copied the public face url and updated the dataset within it. However this didn't work. We then went back to the working example provided by the Data Systems team and spotted that the version had been dropped to 1.0.0 for Water Management.
    
    Using that got the Water Management query working. However today we tried running the public face service manually and again got no result. So on the off chance we tried changing the version to 1.0.0 and voila, we have a result!
    
    Finally changing the version seems to change the format of the result, hence we also had to tweak how we parse the xml 😩!

--- lib/defra_ruby/area/services/public_face_area_service.rb
+++ lib/defra_ruby/area/services/public_face_area_service.rb
@@ -11,12 +11,12 @@ module DefraRuby
 
       def response_xml_path
         type_name = "Administrative_Boundaries_Environment_Agency_and_Natural_England_Public_Face_Areas"
-        "//wfs:FeatureCollection/wfs:member/ms:#{type_name}/ms:long_name"
+        "//wfs:FeatureCollection/gml:featureMember/ms:#{type_name}/ms:long_name"
       end
 
       def url
         # rubocop:disable Metrics/LineLength
-        "https://environment.data.gov.uk/spatialdata/administrative-boundaries-environment-agency-and-natural-england-public-face-areas/wfs?SERVICE=WFS&VERSION=2.0.0&REQUEST=GetFeature&typeNames=ms:Administrative_Boundaries_Environment_Agency_and_Natural_England_Public_Face_Areas&propertyName=long_name&SRSName=EPSG:27700&Filter=(<Filter><Intersects><PropertyName>SHAPE</PropertyName><gml:Point><gml:coordinates>#{easting},#{northing}</gml:coordinates></gml:Point></Intersects></Filter>)"
+        "https://environment.data.gov.uk/spatialdata/administrative-boundaries-environment-agency-and-natural-england-public-face-areas/wfs?SERVICE=WFS&VERSION=1.0.0&REQUEST=GetFeature&typeNames=ms:Administrative_Boundaries_Environment_Agency_and_Natural_England_Public_Face_Areas&propertyName=long_name&SRSName=EPSG:27700&Filter=(<Filter><Intersects><PropertyName>SHAPE</PropertyName><gml:Point><gml:coordinates>#{easting},#{northing}</gml:coordinates></gml:Point></Intersects></Filter>)"
         # rubocop:enable Metrics/LineLength
       end
 
