712623f06247bc53ae4f3d66c9dbdd52bb2db4d2
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2019-08-04 10:54:10 +0100

    Move request error tests to shared example
    As the request error tests would be exactly the same in both service specs, moved them to a shared example to avoid any duplication.

--- spec/defra_ruby/area/services/public_face_area_service_spec.rb
+++ spec/defra_ruby/area/services/public_face_area_service_spec.rb
@@ -58,39 +58,7 @@ module DefraRuby
 
         end
 
-        context "when there is a problem with the Web Feature Service" do
-          before(:each) { VCR.turn_off! }
-          after(:each) { VCR.turn_on! }
-
-          let(:host) { "https://environment.data.gov.uk/" }
-          let(:easting) { 408_602.61 }
-          let(:northing) { 257_535.31 }
-
-          context "and the request times out" do
-            before(:each) { stub_request(:any, /.*#{host}.*/).to_timeout }
-
-            it "returns a failed response" do
-              response = described_class.run(easting, northing)
-              expect(response).to be_a(Response)
-              expect(response).to_not be_successful
-              expect(response.area).to be_nil
-              expect(response.error).to_not be_nil
-            end
-          end
-
-          context "and request returns an error" do
-            before(:each) { stub_request(:any, /.*#{host}.*/).to_raise(SocketError) }
-
-            it "returns a failed response" do
-              response = described_class.run(easting, northing)
-              expect(response).to be_a(Response)
-              expect(response).to_not be_successful
-              expect(response.area).to be_nil
-              expect(response.error).to_not be_nil
-            end
-          end
-
-        end
+        include_examples "handle request errors"
       end
     end
   end
